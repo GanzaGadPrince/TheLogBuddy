edc2815c24de0316bde9b9414249f026cd127024
GanzaGaadPrince <princegadganza77@gmail.com>
2025-11-25 12:01:05 -0700

    Connection check

--- courses.php
+++ courses.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include("nav.php");
 if(!isset($_SESSION['userid'])){
     $userid='0';
--- login.php
+++ login.php
@@ -1,5 +1,6 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 
 $connection=mysqli_connect("sql5.freesqldatabase.com","sql5809449","XFZKmg35Fl","sql5809449","3306");
